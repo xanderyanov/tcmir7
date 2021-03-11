7e20f2f11bc45c948f1535e7195cefcb149ec15a
xanderyanov <xanderyanov@gmail.com>
2021-03-11 17:36:32 +0400

    index2

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,17 @@
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
+
+
 //START прокрутка к якорям на лендинге - функция сама
 var DenyScroll = false;
 function l1ScrollTo() {
@@ -12,10 +26,10 @@ function l1ScrollTo() {
         $this.parent('li').addClass('active');
     }
     let destination = $(elementClick).offset().top;
-    let l1headerHeight = $(".l1_header__left").outerHeight();
+    let l1headerHeight = $(".l1_header__left").outerHeight() + 20;
     DenyScroll = true;
     $("html:not(:animated),body:not(:animated)").animate(
-        { scrollTop: destination - 20 },
+        { scrollTop: destination - l1headerHeight },
         { 
             complete: function() { DenyScroll = false; },
             duration: 1100
